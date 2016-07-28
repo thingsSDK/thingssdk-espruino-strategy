96fdcbb5166e46687839bbd6acd7fa262e725ff0
Andrew Chalkley <andrew@chalkley.org>
2016-07-27 19:37:35 -0700

    Make it fail!

--- test/test_build.js
+++ test/test_build.js
@@ -5,17 +5,17 @@ const assert = require('chai').assert;
 describe('build(entry)', function() {
     describe('when entry is a valid source file', function() {
         it('should return valid ES5 code', function() {
-
+            assert(false);
         });
     });
     describe('when entry is a invalid source file', function() {
         it('should throw an error', function() {
-
+            assert(false);
         });
     });
     describe('when entry is an invalid path file', function() {
         it('should throw an error', function() {
-
+            assert(false);
         });
     });
 });
