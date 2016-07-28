292a489d379938dd712bcc267d14e0d809163a90
Andrew Chalkley <andrew@chalkley.org>
2016-07-27 19:36:05 -0700

    Started tests for collaborators

---
+++ test/test_build.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const assert = require('chai').assert;
+
+describe('build(entry)', function() {
+    describe('when entry is a valid source file', function() {
+        it('should return valid ES5 code', function() {
+
+        });
+    });
+    describe('when entry is a invalid source file', function() {
+        it('should throw an error', function() {
+
+        });
+    });
+    describe('when entry is an invalid path file', function() {
+        it('should throw an error', function() {
+
+        });
+    });
+});
