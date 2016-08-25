751ea49a1b82da91f8daacf9726842caf7ba83b3
craigsdennis <craig@teamtreehouse.com>
2016-08-24 23:17:30 -0700

    Adds listening code.  Rough WIP

--- upload.js
+++ upload.js
@@ -1,6 +1,8 @@
 /* Upload file */
 'use strict';
 const espruino = require('espruino');
+// FIXME:csd How to make this pinned to same as Espruino?
+const SerialPort = require('serialport');
 
 /**
  * Espruino libraries use console.log directly.  This silences code in `fn` until the `callback` is called.
@@ -21,11 +23,23 @@ function uploadToDevice(device, code) {
             espruino.sendCode(device.port, code, () => {
                 unmute();
                 console.log(`Code sent to ${device.port}`);
+                // This waits for the port to actually close, before listening
+                // FIXME:csd This is similar to what is happening in Espruino.  See "disconnected" processor.
+                setTimeout(() => listen(device), 2000);
             });
         });
     });
-
 }
+
+function listen(device) {
+    let terminal = new SerialPort(device.port, {baudRate: device.baud_rate});
+    terminal.on('open', () => console.log('Opened'));
+    terminal.on('data', (data) => {
+        // Also lifted from Espruino.
+        process.stdout.write(String.fromCharCode.apply(null, data));
+    });
+}
+
 
 module.exports = function upload(devices) {
     let espruinoDevices = Object.keys(devices)
