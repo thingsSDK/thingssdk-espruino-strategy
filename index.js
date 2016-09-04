10b6441f6ac3aa9f399e2cd1dcc71a9a7ec881cf
craigsdennis <craig@teamtreehouse.com>
2016-09-04 00:12:28 -0700

    Switches to just use Espruino object and adds input

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const build = require('./build');
 const upload = require('./upload');
+const repl = require('./repl');
 
 module.exports = {
     build,
-    upload
+    upload,
+    repl
 };
--- upload.js
+++ upload.js
@@ -1,8 +1,7 @@
 /* Upload file */
 'use strict';
 const espruino = require('espruino');
-// FIXME:csd How to make this pinned to same as Espruino?
-const SerialPort = require('serialport');
+const repl = require('./repl');
 
 /**
  * Espruino libraries use console.log directly.  This silences code in `fn` until the `callback` is called.
@@ -25,21 +24,11 @@ function uploadToDevice(device, code) {
                 console.log(`Code sent to ${device.port}`);
                 // This waits for the port to actually close, before listening
                 // FIXME:csd This is similar to what is happening in Espruino.  See "disconnected" processor.
-                setTimeout(() => listen(device), 2000);
+                setTimeout(() => repl(device), 2000);
             });
         });
     });
 }
-
-function listen(device) {
-    let terminal = new SerialPort(device.port, {baudRate: device.baud_rate});
-    terminal.on('open', () => console.log('Opened'));
-    terminal.on('data', (data) => {
-        // Also lifted from Espruino.
-        process.stdout.write(String.fromCharCode.apply(null, data));
-    });
-}
-
 
 module.exports = function upload(devices) {
     let espruinoDevices = Object.keys(devices)
