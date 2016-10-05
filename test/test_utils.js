67491e5a4d3f1cef66cda167847ae14def5be133
craigsdennis <craig@teamtreehouse.com>
2016-10-04 22:56:46 -0700

    Adds mocked spawn for cli mocking

--- test/test_utils.js
+++ test/test_utils.js
@@ -1,8 +1,12 @@
 'use strict';
-const assert = require('chai').assert;
-const utils = require('../utils');
 
-describe('filterDevices', () => {
+const assert = require('chai').assert;
+const mockSpawn = require('mock-spawn');
+const proxyquire = require('proxyquire');
+const utils = require('../utils');
+describe('filterDevices(devices)', () => {
+
+
     it('keeps only espruino runtime', () => {
         let devices = {
           "port1": {"runtime": "espruino"},
@@ -22,5 +26,42 @@ describe('filterDevices', () => {
         let results = utils.filterDevices(devices);
 
         assert.equal(results[0].port, "port1");
+    });
+});
+
+describe('runEspruino(device, ...cmdLineArgs)', () => {
+    let mockedUtils;
+    let mockedSpawn;
+    let device = {
+        "port": "port1",
+        "runtime": "espruino",
+        "baud_rate": "9600"
+    };
+
+    beforeEach(() => {
+        mockedSpawn = mockSpawn();
+        mockedUtils = proxyquire('../utils', {
+            'child_process': {'spawn': mockedSpawn}
+        });
+    });
+
+    it('spawns a new espruino-cli with no args', () => {
+        mockedUtils.runEspruino(device);
+
+        assert.equal(mockedSpawn.calls.length, 1);
+        let call = mockedSpawn.calls[0];
+        assert.equal(call.command, 'node', "node is called");
+        // String contains (avoiding path probs in test)
+        assert.include(call.args[0], 'espruino-cli', "espruino-cli script is called");
+        assert.include(call.args, "9600", "baud rate is passed");
+        assert.include(call.args, "port1", "port is passed");
+    });
+
+    it('spawns with additional params', () => {
+        mockedUtils.runEspruino(device, 'p1', 'p2');
+
+        let args = mockedSpawn.calls[0].args;
+        assert.include(args, 'p1');
+        assert.include(args, 'p2');
     });
 });
