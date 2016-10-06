4670329a412bd622988fed3d0bced77202f88b20
craigsdennis <craig@teamtreehouse.com>
2016-10-05 20:30:22 -0700

    Adds upload test

--- upload.js
+++ upload.js
@@ -3,16 +3,12 @@
 const path = require('path');
 const utils = require('./utils');
 
-function uploadToDevice(device, filePath) {
-    console.log(`Sending code to device - ${device.port} @ ${device.baud_rate} baud...`);
-
-    utils.runEspruino(device, '-c', filePath);
-}
-
 module.exports = function upload(devices, payload, next) {
     let espruinoDevices = utils.filterDevices(devices);
     espruinoDevices.forEach(device => {
-        return uploadToDevice(device, path.join(payload.buildDir, 'espruino-generated.js'));
+        let filePath = path.join(payload.buildDir, 'espruino-generated.js');
+        console.log(`Sending code to device - ${device.port} @ ${device.baud_rate} baud...`);
+        utils.runEspruino(device, '-c', filePath);
     });
     next();
 };
